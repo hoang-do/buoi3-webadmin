52c71fbcbaf2845eb2af2e92ff833c19be8ffc1a
hoang-do <1851171950@e.tlu.edu.vn>
2022-01-19 14:23:28 +0700

    fix

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,43 +1,10 @@
-import { createContext, useEffect, useReducer } from "react";
-import { authReducer } from "../reducers/authReducer";
+import { createContext } from "react";
 import { apiUrl, LOCAL_STORAGE_TOKEN_NAME } from "./constants";
 import axios from "axios";
-import setAuthToken from "../utils/setAuthToken";
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
-//   const [authState, dispatch] = useReducer(authReducer, {
-//     authLoading: true,
-//     isAuthenticated: false,
-//     user: null,
-//   });
-
-  // const loadUser = async () => {
-  // 	if (localStorage[LOCAL_STORAGE_TOKEN_NAME]) {
-  // 		setAuthToken(localStorage[LOCAL_STORAGE_TOKEN_NAME])
-  // 	}
-
-  // 	try {
-  // 		const response = await axios.get(`${apiUrl}/auth`)
-  // 		if (response.data.success) {
-  // 			dispatch({
-  // 				type: 'SET_AUTH',
-  // 				payload: { isAuthenticated: true, user: response.data.user }
-  // 			})
-  // 		}
-  // 	} catch (error) {
-  // 		localStorage.removeItem(LOCAL_STORAGE_TOKEN_NAME)
-  // 		setAuthToken(null)
-  // 		dispatch({
-  // 			type: 'SET_AUTH',
-  // 			payload: { isAuthenticated: false, user: null }
-  // 		})
-  // 	}
-  // }
-
-  // useEffect(() => loadUser(), [])
-
   //Login
   const loginUser = async (userForm) => {
     try {
@@ -46,7 +13,7 @@ const AuthContextProvider = ({ children }) => {
         localStorage.setItem(
           LOCAL_STORAGE_TOKEN_NAME,
           response.data.accessToken
-        );  
+        );
       return response.data;
     } catch (error) {
       if (error.response.data) return error.response.data;
@@ -60,7 +27,7 @@ const AuthContextProvider = ({ children }) => {
   };
 
   // Context data
-  const authContextData = { loginUser,logoutUser };
+  const authContextData = { loginUser, logoutUser };
 
   //Return provider
   return (
--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,14 +1,11 @@
-export const apiUrl =
-  process.env.NODE_ENV !== "production"
-    ? "https://nws-management.herokuapp.com"
-    : "somedeployedURL";
+export const apiUrl = "https://nws-management.herokuapp.com";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "user-info";
 
-export const DELETE_DEPARTMENT = 'DELETE_DEPARTMENT'
-export const ADD_DEPARTMENT = 'ADD_DEPARTMENT'
-export const UPDATE_DEPARTMENT = 'UPDATE_DEPARTMENT'
-export const FIND_DEPARTMENT = 'FIND_DEPARTMENT'
+export const DELETE_DEPARTMENT = "DELETE_DEPARTMENT";
+export const ADD_DEPARTMENT = "ADD_DEPARTMENT";
+export const UPDATE_DEPARTMENT = "UPDATE_DEPARTMENT";
+export const FIND_DEPARTMENT = "FIND_DEPARTMENT";
 
 export const DELETE_EMPLOYEE = "DELETE_EMPLOYEE";
 export const ADD_EMPLOYEE = "ADD_EMPLOYEE";
